5a855140dcbfa9018f681564d959b35ed23f88b8
casper88 <casper88tw@gmail.com>
2016-09-04 11:35:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ var bg=document.createElement("img");
 bg.src="images/map.png";
 var canvas=document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d")
+function draw(){
+  ctx.drawImage(bgImg,0,0);
+}
+draw()
+setTimeout(draw,1000)
