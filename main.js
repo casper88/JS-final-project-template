1f295c6810d65f1261114911a6267aa9f75bc22a
casper88 <casper88tw@gmail.com>
2016-09-04 11:37:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var bg=document.createElement("img");
 bg.src="images/map.png";
-var canvas=document.getElementById("game-canvas");
+var canvas=document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d")
 function draw(){
   ctx.drawImage(bgImg,0,0);
