171d807a531195b335e123437f54c46f49a4f945
casper88 <casper88tw@gmail.com>
2016-09-04 11:48:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var bg=document.createElement("img");
 bg.src="images/map.png";
+var ani=document.createElement("img");
+ani.src="image/slime.gif"
 var canvas=document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d")
 function draw(){
   ctx.drawImage(bg,0,0);
+  ctx.drawImage(ani,0,0);
 }
 
 setTimeout(draw,1000)
+
