55149b6d0dc49f15d839eea5f3b173081c85b6c6
casper88 <casper88tw@gmail.com>
2016-10-02 11:33:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,9 @@ var canvas=document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bg,1,1);
-  ctx.drawImage(aniImg,0,0);
   ctx.drawImage(towerbtn,660,500);
+  ctx.drawImage(aniImg,0,0);
+  
 }
 
 setInterval(draw,1000);
