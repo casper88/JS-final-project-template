6cb5e50a48bf3fac3082668b15b62b2c214e66cc
casper88 <casper88tw@gmail.com>
2016-10-16 11:11:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,140 @@
-var fps=60;
-var bg=document.createElement("img");
-bg.src="images/map.png";
-var aniImg=document.createElement("img");
-aniImg.src="images/slime.gif";
-var towerbtn=document.createElement("img");
-towerbtn.src="images/tower-btn.png";
-var ani={
-  x:0,
-  y:0
-};
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
+var eImg = document.createElement("img");
+eImg.src = "images/rukia.gif";
+var tImg = document.createElement("img");
+tImg.src = "images/tower-btn.png";
 var towerImg = document.createElement("img");
 towerImg.src = "images/tower.png";
-var cursor={x:0,y:0};
-var canvas=document.getElementById("game_canvas");
+var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
-function draw(){
-  ctx.drawImage(bg,1,1);
-  ctx.drawImage(towerbtn,540,380,100,100);
- 
-  
+var fps = 60;
+var enemyPath = [
+  {x: 96, y: 64},
+  {x: 384, y: 64},
+  {x: 384, y: 192},
+  {x: 224, y: 192},
+  {x: 224, y: 320},
+  {x: 544, y: 320},
+  {x: 544, y: 96},
+];
+
+function Enemy() {
+  this.x = 96;
+  this.y = 400;
+  this.v = [1,1]
+  this.speedX = 0;
+  this.speedY = -64
+  this.pathDes = 0;
+  this.move = function(){
+    if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, 64/fps, 64/fps)) {
+      this.x = enemyPath[this.pathDes].x;
+      this.y = enemyPath[this.pathDes].y;
+      
+      if(this.x == enemyPath[this.pathDes+1].x) {
+        if(this.y > enemyPath[this.pathDes+1].y) {
+          this.speedY = -64;
+          this.speedX = 0;
+        } else {
+          this.speedY = 64;
+          this.speedX = 0;
+        }
+      } else if (this.y == enemyPath[this.pathDes+1].y) {
+        if(this.x > enemyPath[this.pathDes+1].x) {
+          this.speedY = 0;
+          this.speedX = -64;
+        } else {
+          this.speedY = 0;
+          this.speedX = 64;
+        }
+      }
+      
+      this.pathDes += 1;
+      
+    } else {
+      this.x += this.speedX/fps;
+      this.y += this.speedY/fps;
+    }
+  }
 }
 
-setInterval(draw,1000);
+var cursor = {x: 0, y: 0};
+var isBuilding = false;
+var enemies = [];
+var clock = 0;
 
-$("#game_canvas").mousemove(function(event){
-  console.log("x:"+ event.offsetX+",y:",event.offsetY);
-  cursor.x=event.offsetX-(event.offsetX%32);
-  cursor.y=event.offsetY-(event.offsetY%32);
+function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight){
+  if(pointX >= targetX && pointX <= targetX + targetWidth && pointY >= targetY && pointY <= targetY + targetHeight) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function draw(){
+  clock++;
+  
+  ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(tImg,640-64,480-64,64,64);
+  ctx.drawImage(towerImg, tower.x, tower.y);
+  
+  for(var i = 0; i < enemies.length; i++){
+    enemies[i].move()
+    ctx.drawImage(eImg,enemies[i].x,enemies[i].y);
+  }
+
+  if(isBuilding == true) {
+    ctx.drawImage(towerImg, cursor.x, cursor.y);
+  }
+  
+  if((clock % 80) == 0) {
+    var enemy = new Enemy();
+    enemies.push(enemy);
+  }
+}
+setInterval(draw,1000/fps);
+$("body").on("keypress",key);
+function key(event){
+  console.log(event.which)
+  if(event.which === 119){
+    enemy.y -= enemy.v[1]
+    enemy.v[1] *= 1.1
+  }
+  if(event.which === 115){
+    enemy.y += enemy.v[1]
+    enemy.v[1] *= 1.1
+  }
+  if(event.which === 100){
+    enemy.x += enemy.v[0]
+    enemy.v[0] *= 1.1
+  }
+  if(event.which === 97){
+    enemy.x -= enemy.v[0]
+    enemy.v[0] *= 1.1
+  }
+}
+
+$("#game-canvas").on("mousemove", function(event) {
+  cursor.x = event.offsetX - (event.offsetX%32);
+  cursor.y = event.offsetY - (event.offsetY%32);
 });
 
-
+var tower = {x:0, y:0};
+$("#game-canvas").on("click", function() {
+  if(cursor.x >= 640-64 && cursor.y >= 480-64) {
+    if(isBuilding == false) {
+      isBuilding = true;
+    } else {
+      isBuilding = false;
+    }
+  } else {
+    console.log(cursor.x);
+    console.log(cursor.y);
+    if(isBuilding == true) {
+      tower.x = cursor.x;
+      tower.y = cursor.y;
+      
+      isBuilding = false;
+    }
+  }
+})
